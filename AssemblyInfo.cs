37310517bc00d64db27963d0af7ddaaf0402c449
Phaedrus3 <131749747+Phaedrus3@users.noreply.github.com>
2023-09-27 17:59:50 -0500

    Delete AssemblyInfo.cs

--- AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using MelonLoader;
-using System.Reflection;
-
-[assembly: AssemblyTitle("IncreaseSaveSlotLimit")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: MelonInfo(typeof(IncreaseSaveSlotLimit.Implementation), "IncreaseSaveSlotLimit", "1.2", "zeobviouslyfakeacc, ds5678, phaedrus")]
-[assembly: MelonGame("Hinterland", "TheLongDark")]
